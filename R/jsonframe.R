8a4af151cdb380b02ee5092abc9694e9881bf2da
Maciej Banas <banasmaciek@gmail.com>
2025-02-28 08:36:27 +0000

    Fix filtering and improve get methods.

--- R/jsonframe.R
+++ R/jsonframe.R
@@ -1,34 +1,43 @@
+# Function to include the JSON frame in the Shiny app
 include_jsonframe <- function() {
-  addResourcePath("www", system.file("www", package = "jsonframe"))
+  shiny::addResourcePath("www", system.file("www", package = "jsonframe"))
   htmltools::tags$head(
     htmltools::tags$script(src = "www/script.js")
   )
 }
+
+# Define the JSONFrame R6 Class
 JSONFrame <- R6::R6Class(
   "JSONFrame",
   public = list(
-    initialize = function(session, input, json_data) {
+    initialize = function(input, json_data, session = shiny::getDefaultReactiveDomain()) {
       private$session <- session
+      private$original_data <- json_data
       session$sendCustomMessage("createJSONFrame", json_data)
     },
     get_json = function(input) {
-      jsonlite::toJSON(input$json_frame)
+      as.character(input$json_frame)
     },
     get_data_frame = function(input) {
       json_frame <- shiny::reactive(input$json_frame)
       data_frame <- purrr::map(json_frame()$data, function(row) {
-        data_row <- data.frame(row)
+        data_row <- data.frame(t(matrix(unlist(row), nrow = length(row))))
         colnames(data_row) <- unlist(json_frame()$columns)
         return(data_row)
       }) |>
-        data.table::rbindlist()
+        data.table::rbindlist(fill = TRUE)
       return(data_frame)
     },
-    filter = function(input, filter_expression) {
-      private$session$sendCustomMessage("filterJSONFrame", filter_expression)
+    filter = function(input, filter_expression, session = shiny::getDefaultReactiveDomain()) {
+      session$sendCustomMessage("filterJSONFrame", list(
+        original_data = private$original_data,
+        filter_expression = filter_expression
+      ))
     }
   ),
   private = list(
-    session = NULL
+    session = NULL,
+    original_data = NULL
   )
 )
+
--- inst/www/script.js
+++ inst/www/script.js
@@ -1,30 +1,27 @@
+// Function to create JSON frame
 function createJSONFrame(jsonString) {
-  let jsonData  = processJSONData(jsonString);
+  let jsonData = processJSONData(jsonString);
   Shiny.setInputValue("json_frame", jsonData);
 }
 
 Shiny.addCustomMessageHandler("createJSONFrame", createJSONFrame);
 
-function filterJSONFrame(filterExpression) {
-  let jsonFrame = Shiny.shinyapp.$inputValues["json_frame"];
-  let filterParameters = parseFilterExpression(filterExpression);
+// Function to filter JSON frame
+function filterJSONFrame(params) {
+  let jsonFrame = processJSONData(params.original_data);
+  let filterParameters = parseFilterExpression(params.filter_expression);
   let jsonFilteredData = filterData(
-    data = jsonFrame.data,
-    columns = jsonFrame.columns,
-    columnName = filterParameters.columnName,
-    condition = filterParameters.condition
-  )
+    jsonFrame.data,
+    jsonFrame.columns,
+    filterParameters.columnName,
+    filterParameters.condition
+  );
   Shiny.setInputValue("json_frame", jsonFilteredData);
 }
 
 Shiny.addCustomMessageHandler("filterJSONFrame", filterJSONFrame);
 
-function getJSONData(inputString) {
-  let jsonString = inputString.replace(/'/g, '"').replace(/(\w+):/g, '"$1":');
-  const jsonData = JSON.parse(jsonString);
-  return(jsonData);
-}
-
+// Helper function to process JSON string
 function processJSONData(inputString) {
   const jsonData = JSON.parse(inputString);
   const columns = new Set();
@@ -35,42 +32,41 @@ function processJSONData(inputString) {
   });
   const colArray = Array.from(columns);
   const rowData = jsonData.map(item => {
-    return colArray.map(col => {
-      if (typeof item[col] === 'object') return JSON.stringify(item[col]);
-      return item[col] !== undefined ? item[col] : '';
-    });
+    return colArray.map(col => item[col] !== undefined ? item[col] : '');
   });
 
-  return{ columns: colArray, data: rowData }
+  return { columns: colArray, data: rowData };
 }
 
+// Helper function to filter data
 function filterData(data, columns, columnName, condition) {
-    const columnIndex = columns.indexOf(columnName);
-    if (columnIndex === -1) {
-        throw new Error(`Column '${columnName}' not found.`);
-    }
-    data = data.filter(row => condition(row[columnIndex]));
-    return{columns, data}
+  const columnIndex = columns.indexOf(columnName);
+  if (columnIndex === -1) {
+    throw new Error(`Column '${columnName}' not found.`);
+  }
+  data = data.filter(row => condition(row[columnIndex]));
+  return { columns, data };
 }
 
+// Helper function to parse filter expression
 function parseFilterExpression(expression) {
-    const match = expression.match(/(\w+)\s*(==|>=|<=|>|<)\s*('?[^']+'?)/);
-    if (!match) {
-        throw new Error('Invalid filter expression');
-    }
-    const [, columnName, operator, value] = match;
+  const match = expression.match(/(\w+)\s*(==|>=|<=|>|<)\s*('?[^']+'?)/);
+  if (!match) {
+    throw new Error('Invalid filter expression');
+  }
+  const [, columnName, operator, value] = match;
 
-    // Determine the condition function based on the operator
-    const conditionFunctions = {
-        '==': val => val == (value.startsWith("'") ? value.slice(1, -1) : value),
-        '>=': val => val >= Number(value),
-        '<=': val => val <= Number(value),
-        '>': val => val > Number(value),
-        '<': val => val < Number(value)
-    };
-    
-    return {
-        columnName,
-        condition: conditionFunctions[operator]
-    }
+  // Determine the condition function based on the operator
+  const conditionFunctions = {
+    '==': val => val == (value.startsWith("'") ? value.slice(1, -1) : value),
+    '>=': val => val >= Number(value),
+    '<=': val => val <= Number(value),
+    '>': val => val > Number(value),
+    '<': val => val < Number(value)
+  };
+
+  return {
+    columnName,
+    condition: conditionFunctions[operator]
+  };
 }
