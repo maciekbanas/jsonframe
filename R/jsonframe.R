fa46ba6bf2b7ca3ba25f22f675fa39bc86dc5487
Maciej Banas <banasmaciek@gmail.com>
2025-02-28 14:53:51 +0000

    Remove superfluous. Get_data_frame() is not needed for the time being.

--- R/jsonframe.R
+++ R/jsonframe.R
@@ -1,4 +1,3 @@
-# Function to include the JSON frame in the Shiny app
 include_jsonframe <- function() {
   shiny::addResourcePath("www", system.file("www", package = "jsonframe"))
   htmltools::tags$head(
@@ -11,22 +10,11 @@ JSONFrame <- R6::R6Class(
   "JSONFrame",
   public = list(
     initialize = function(input, json_data, session = shiny::getDefaultReactiveDomain()) {
-      private$session <- session
       private$original_data <- json_data
       session$sendCustomMessage("createJSONFrame", json_data)
     },
     get_json = function(input) {
       as.character(input$json_frame)
-    },
-    get_data_frame = function(input) {
-      json_frame <- shiny::reactive(input$json_frame)
-      data_frame <- purrr::map(json_frame()$data, function(row) {
-        data_row <- data.frame(t(matrix(unlist(row), nrow = length(row))))
-        colnames(data_row) <- unlist(json_frame()$columns)
-        return(data_row)
-      }) |>
-        data.table::rbindlist(fill = TRUE)
-      return(data_frame)
     },
     filter = function(input, filter_expression, session = shiny::getDefaultReactiveDomain()) {
       session$sendCustomMessage("filterJSONFrame", list(
@@ -36,8 +24,6 @@ JSONFrame <- R6::R6Class(
     }
   ),
   private = list(
-    session = NULL,
     original_data = NULL
   )
 )
-
