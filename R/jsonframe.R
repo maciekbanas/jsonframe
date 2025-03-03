1b3db2c8491f79228f36f2337aad169e14a05c2b
Maciej Banas <banasmaciek@gmail.com>
2025-03-03 10:11:23 +0000

    Fix checks.

--- R/jsonframe.R
+++ R/jsonframe.R
@@ -1,3 +1,8 @@
+#' @importFrom R6 R6Class
+
+#' @title Link JS code.
+#' @name include_jsonframe
+#' @export
 include_jsonframe <- function() {
   shiny::addResourcePath("www", system.file("www", package = "jsonframe"))
   htmltools::tags$head(
@@ -5,17 +10,28 @@ include_jsonframe <- function() {
   )
 }
 
-# Define the JSONFrame R6 Class
+#' @title A `JSONFrame` class.
+#' @export
 JSONFrame <- R6::R6Class(
   "JSONFrame",
   public = list(
+    #' @description Send `JSON` data to the browser.
+    #' @param input A `Shiny` input parameter.
+    #' @param json_data A character - should mirror JSON data structure.
+    #' @param session A `Shiny` session parameter.
     initialize = function(input, json_data, session = shiny::getDefaultReactiveDomain()) {
       private$original_data <- json_data
       session$sendCustomMessage("createJSONFrame", json_data)
     },
+    #' @description Retrieve `JSON` from the browser.
+    #' @param input A `Shiny` input parameter.
     get_json = function(input) {
       as.character(input$json_frame)
     },
+    #' @description Send `JSON` data to the browser.
+    #' @param input A `Shiny` input parameter.
+    #' @param filter_expression A character, filtering expression (e.g. "{var} >= 0").
+    #' @param session A `Shiny` session parameter.
     filter = function(input, filter_expression, session = shiny::getDefaultReactiveDomain()) {
       session$sendCustomMessage("filterJSONFrame", list(
         original_data = private$original_data,
--- tests/testthat/test-jsonframe.R
+++ tests/testthat/test-jsonframe.R
@@ -1,22 +1,21 @@
-library(testthat)
-library(shiny)
-library(jsonlite)
-library(R6)
-
-# Test JSON data
 jsondata <- '[{"name": "Alice", "age": 25, "city": "New York"},
               {"name": "Bob", "age": 30, "city": "San Francisco"},
               {"name": "Charlie", "age": 35, "city": "Los Angeles"},
               {"name": "David", "age": 40, "city": "Chicago"},
               {"name": "Eve", "age": 28, "city": "Houston"}]'
 
-# Create a mock input
 mock_input <- shiny::reactiveValues(json_frame = NULL)
 
-# Create a mock session
+MockShinySession <- R6::R6Class(
+  "MockShinySession",
+  public = list(
+    sendCustomMessage = function(message, data) {
+    }
+  )
+)
+
 mock_session <- MockShinySession$new()
 
-# Tests for JSONFrame R6 class methods
 test_that("JSONFrame initializes correctly", {
   my_json_frame <- JSONFrame$new(input = mock_input, json_data = jsondata, session = mock_session)
   expect_equal(my_json_frame$.__enclos_env__$private$original_data, jsondata)
@@ -25,5 +24,5 @@ test_that("JSONFrame initializes correctly", {
 test_that("get_json returns the correct json data", {
   my_json_frame <- JSONFrame$new(input = mock_input, json_data = jsondata, session = mock_session)
   shiny::isolate({ mock_input$json_frame <- jsondata })
-  expect_equal(isolate(my_json_frame$get_json(mock_input)), jsondata)
+  expect_equal(shiny::isolate(my_json_frame$get_json(mock_input)), jsondata)
 })
