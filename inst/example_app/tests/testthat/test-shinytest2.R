0caf67e4ab3e07f267d1a9a31cdaa23787ee2114
Maciej Banas <banasmaciek@gmail.com>
2025-05-23 07:04:10 +0000

    Added basic test checking if whole data is loaded.

--- inst/example_app/tests/testthat/test-shinytest2.R
+++ inst/example_app/tests/testthat/test-shinytest2.R
@@ -1,5 +1,14 @@
 library(shinytest2)
 
+test_that("{shinytest2} recording: jsonframe is registered", {
+  app <- AppDriver$new(name = "Apply age filter")
+  json_string <- app$get_value(output = "json_string")
+  expect_equal(
+    json_string,
+    "[{\"name\":\"Alice\",\"age\":25,\"city\":\"New York\"},{\"name\":\"Bob\",\"age\":30,\"city\":\"San Francisco\"},{\"name\":\"Charlie\",\"age\":35,\"city\":\"Los Angeles\"},{\"name\":\"David\",\"age\":40,\"city\":\"Chicago\"},{\"name\":\"Eve\",\"age\":28,\"city\":\"Houston\"},{\"name\":\"Frank\",\"age\":45,\"city\":\"Phoenix\"},{\"name\":\"Grace\",\"age\":32,\"city\":\"Philadelphia\"},{\"name\":\"Heidi\",\"age\":38,\"city\":\"San Antonio\"},{\"name\":\"Ivan\",\"age\":50,\"city\":\"San Diego\"},{\"name\":\"Judy\",\"age\":22,\"city\":\"Dallas\"},{\"name\":\"Karl\",\"age\":28,\"city\":\"San Jose\"},{\"name\":\"Laura\",\"age\":31,\"city\":\"Austin\"},{\"name\":\"Mallory\",\"age\":37,\"city\":\"Jacksonville\"},{\"name\":\"Nathan\",\"age\":42,\"city\":\"Fort Worth\"}]"
+  )
+})
+
 test_that("{shinytest2} recording: Apply age >= 30 filter", {
   app <- AppDriver$new(name = "Apply age filter")
   app$set_inputs(filter_expression = "age >= 30")
@@ -22,7 +31,7 @@ test_that("{shinytest2} recording: Apply age >= 35 filter", {
   )
 })
 
-test_that("{shinytest2} recording: Apply age >= 35 filter", {
+test_that("{shinytest2} recording: Apply age == 50 filter", {
   app <- AppDriver$new(name = "Apply age filter")
   app$set_inputs(filter_expression = "age == 50")
   app$click("filter_btn")
